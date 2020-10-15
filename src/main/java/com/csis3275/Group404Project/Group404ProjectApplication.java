0413eb553eba3787d3ab92b8912e03fcb47a2c45
Francis Poirier <poirierf@student.douglascollege.ca>
2020-10-15 15:16:18 -0700

    Initial creation of SpringMVC app.

---
+++ src/main/java/com/csis3275/Group404Project/Group404ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.csis3275.Group404Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group404ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group404ProjectApplication.class, args);
+	}
+
+}
