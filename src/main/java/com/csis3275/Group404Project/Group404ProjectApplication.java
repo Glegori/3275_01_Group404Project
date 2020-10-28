65859fcca3c8477e3d5dbd067b64185442581076
Shubham Kapoor <shubhamkapoor9778@gmail.com>
2020-10-28 12:22:49 -0700

    Added Login Screen and established connection to the database for authorization

--- src/main/java/com/csis3275/Group404Project/Group404ProjectApplication.java
+++ src/main/java/com/csis3275/Group404Project/Group404ProjectApplication.java
@@ -2,8 +2,10 @@ package com.csis3275.Group404Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class Group404ProjectApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/csis3275/Group404Project/models/LoginUserDetails.java
@@ -0,0 +1,73 @@
+package com.csis3275.Group404Project.models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+
+public class LoginUserDetails implements UserDetails {
+
+    private String userName;
+    private String password;
+    private String reportTo;
+    private String userType;
+    private String total;
+
+    private SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
+
+
+
+
+    public LoginUserDetails(USER_404_PROJECT user){
+        this.userName = user.getUserName();
+        this.password = user.getPassword();
+        this.reportTo = user.getReportTO();
+        this.userType = user.getUserType();
+        this.total = user.getTotal();
+
+        if(userType.equals("admin")){
+            authority = new SimpleGrantedAuthority("ROLE_ADMIN");
+        }
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(authority);
+    }
+
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+//    }
+
+    public String getPassword() {
+        System.out.println("Password is" + this.password);
+        return this.password;
+    }
+
+    public String getUsername() {
+        System.out.println("I am in details");
+        System.out.println(this.userName);
+        return this.userName;
+    }
+
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    public boolean isEnabled() {
+        return true;
+    }
+}
