4d5d7fd6ea95916907a3e2da67cfbbbf132f7f8f
Greg <billingsgregory2@gmail.com>
2020-11-11 22:56:13 -0800

    adding in delete operation in expenseDAO for System test CRUD

--- src/main/java/com/csis3275/Group404Project/dao/expenseDAO.java
+++ src/main/java/com/csis3275/Group404Project/dao/expenseDAO.java
@@ -25,6 +25,7 @@ public class expenseDAO {
 	private final String SQL_GET_ALL = "select * from EXPENSE_404_project";
 	private final String SQL_GET_ALL_EXPENSES_BY_USERNAME = "select * from EXPENSE_404_project where USER = ?";
 	private final String SQL_GET_REPORTS_FROM = "select * from USER_404_project where USERNAME = ?";
+	private final String SQL_DELETE_BY_EXPENSE_ID = "delete from EXPENSE_404_project where id = ?";
 	private final String SQL_UPDATE_STATUS = "update EXPENSE_404_project SET expenseStatus = ? WHERE id = ?";
 	private final String SQL_GET_EXPENSE_BY_EXPENSETYPE = "SELECT * FROM EXPENSE_404_project WHERE USER = ? AND EXPENSETYPE = ?";
 	private final String SQL_INSERT_EXPENSE = "insert into EXPENSE_404_project(expenseName, expenseCost, date, expenseType, expenseStatus, billImage, user) values(?,?,?,?,?,?,?)";
@@ -64,7 +65,7 @@ public class expenseDAO {
 	public List<Expense> getExpensesByUserName(String USER){
 		return jdbcTemplate.query(SQL_GET_ALL_EXPENSES_BY_USERNAME, new Object[] {USER} , new ExpenseMapper());
 	}
-	
+
 	public List<Expense> getExpensesByUserNameAndExpenseType(String USER, String EXPENSETYPE){
 		System.out.println("System is searching expense type = "+ EXPENSETYPE + " into the Database");
 		return jdbcTemplate.query(SQL_GET_EXPENSE_BY_EXPENSETYPE, new Object[] {USER, EXPENSETYPE} , new ExpenseMapper());
@@ -83,6 +84,10 @@ public class expenseDAO {
 	}
 	public List<Map<String, Object>> getTotalCostByUser() {
 		return jdbcTemplate.queryForList(SQL_GET_TOTAL_COST_BY_USER);
+	}
+	public boolean deleteExpense(Expense expense){
+		System.out.println("The ID of the expense your deleting is: " + expense.getId());
+		return jdbcTemplate.update(SQL_DELETE_BY_EXPENSE_ID,  expense.getId()) > 0;
 	}
 }
 
